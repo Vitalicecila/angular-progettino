4485aa59422252799a4e7bd80693510a5bb87cc7
Vitalicecila <vitalialice99@gmail.com>
2021-06-09 13:19:11 +0200

    1 file modified

--- src/app/chiave.service.ts
+++ src/app/chiave.service.ts
@@ -9,7 +9,7 @@ export class ChiaveService {
   apiKEY: string = '';
   apiURL: string =
     'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
-    
+
   constructor(private service: HttpClient) {}
   public getData() {
     let promise = fetch(this.apiURL + '/get?key=' + this.apiKEY);
